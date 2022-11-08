0a9cb14d5678f6a9d9a418312e9d7871da2fdfad
thisiskshitu <thisiskshitu@yahoo.com>
2022-11-08 21:44:35 +0530

    Restored all successful changes which were lost due to incorrect force pushing which occurred due to folder relocation on local machine

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,28 @@
-document.getElementById("whole-div-id").style.backgroundImage = "url(media/day.png)";
+// function changeBackgroundToDay() {
+//   (document.getElementById("whole-div-id").style.filter = "brightness(3)"),
+//   setTimeout(function () {
+//     (document.getElementById("whole-div-id").style.backgroundImage = "url(/../media/day.png)");
+//   }, 50),
+//     setTimeout(function () {
+//       document.getElementById("whole-div-id").style.filter = "brightness(100%)";
+//     }, 100);
+// }
+// function changeBackgroundToNight() {
+//   (document.getElementById("whole-div-id").style.filter = "brightness(3)"),
+//   setTimeout(function () {
+//     (document.getElementById("whole-div-id").style.backgroundImage = "url(/../media/night.png)");
+//   }, 50);
+//     setTimeout(function () {
+//       document.getElementById("whole-div-id").style.filter = "brightness(100%)";
+//     }, 100);
+// }
 
 function changeBackgroundToDay()
 {
-  document.getElementById("whole-div-id").style.filter = "blur(5px)";
-  setTimeout
-  (
-    function changeBackgroundToDayBlur0()
-    {
-      document.getElementById("whole-div-id").style.filter = "blur(0px)";
-    }, 200
-  )
-  setTimeout
-  (
-    function changeBackgroundToDayDisplay()
-    {
-      document.getElementById("whole-div-id").style.backgroundImage = "url(media/day.png)";
-      // document.getElementById("btn-id1").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id2").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id3").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id4").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id5").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id6").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id7").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id8").style.background = "rgb(0, 0, 0, 0.50)";
-
-      // document.getElementById("btn-id1").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id2").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id3").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id4").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id5").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id6").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id7").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id8").style.background = "rgb(0, 0, 0, 0.50)";
-    }, 100
-  )
+  document.getElementById("whole-div-id").style.backgroundImage = "url(/../media/day.png)";
 }
 
 function changeBackgroundToNight()
 {
-  // document.getElementById("whole-div-id").style.filter = "brightness(0)";
-  document.getElementById("whole-div-id").style.filter = "blur(5px)";
-  setTimeout
-  (
-    function changeBackgroundToNightBlur0()
-    {
-      // document.getElementById("whole-div-id").style.filter = "brightness(100%)";
-      document.getElementById("whole-div-id").style.filter = "blur(0px)"; 
-    }, 200
-  )
-  setTimeout
-  (
-    function changeBackgroundToNightDisplay()
-    {
-      document.getElementById("whole-div-id").style.backgroundImage = "url(media/night.png)";
-      // document.getElementById("btn-id1").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id2").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id3").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id4").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id5").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id6").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id7").style.background = "rgb(0, 0, 0, 0.50)";
-      // document.getElementById("btn-id8").style.background = "rgb(0, 0, 0, 0.50)";
-    }, 100
-  )
+  document.getElementById("whole-div-id").style.backgroundImage = "url(/../media/night.png)";
 }
